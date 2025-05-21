0f5e54b8b874cfdd25d86ce7d7d435493223035b
h7adithya7532 <152002126+h7adithya7532@users.noreply.github.com>
2025-05-21 15:55:07 +0530

    Add files via upload

---
+++ project stock.sql
@@ -0,0 +1,69 @@
+Create Table importstockdata(
+	stock_name VARCHAR(100),
+	isin Varchar(20),
+	quantity INT,
+	avg_buy_price FLOAT,
+	buy_value FLOAT,
+	avg_sell_price FLOAT,
+	sell_value FLOAT,
+	realized_pnl FLOAT,
+	realized_pnl_percentage FLOAT
+);
+--Imported the data from CSV file of the stock portfolio of the Stock p&l from April-1-2025 to May-20-2025
+--Right click on the StockAnalysisDB
+--Select >Task>Import Flat file
+--The importing File should be in the CSV format
+
+--Adding a column name Id with Alter
+ALTER TABLE importstockdata
+ADD Id INT IDENTITY(1,1) PRIMARY KEY;
+
+--Checking the Table By Selecting it
+Select*
+From dbo.importstockdata;
+
+--Deleteing the last row as it is used to calculate total's
+Delete From importstockdata
+where id=31;
+
+--Total Investment
+SELECT SUM(buy_value) AS Total_Investment
+FROM importstockdata;
+
+--Total Sell Value
+SELECT SUM(sell_value) AS Total_sell_value
+FROM importstockdata;
+
+--Net Realized Profit&Loss
+SELECT SUM(realized_pnl) AS NetProfitLoss
+FROM importstockdata;
+
+--Top5 profit making stocks
+SELECT TOP 5 stock_name,
+SUM(realized_pnl) AS Profit
+FROM importstockdata
+GROUP BY stock_name
+ORDER BY Profit DESC;
+
+--Worst 5 Performing Stocks
+SELECT TOP 5 stock_name,
+SUM(realized_pnl) AS Loss
+FROM importstockdata
+GROUP BY stock_name
+ORDER BY Loss ASC;
+
+--Calculating ROI(Return On Investment)
+SELECT stock_name,
+       CAST(SUM(CAST(realized_pnl AS FLOAT)) / NULLIF(SUM(CAST(buy_value AS FLOAT)), 0) * 100 AS DECIMAL(10, 2)) AS ROI_Percentage
+FROM importstockdata
+GROUP BY stock_name
+ORDER BY ROI_Percentage DESC;
+
+
+
+
+
+
+
+
+
